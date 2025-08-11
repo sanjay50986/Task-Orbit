3d0b46581e1dba7829764fe69445f37bb8bc8207
Sanjay Jangid <sanjay50986@gmail.com>
2025-08-11 23:13:40 +0530

    feat(workspace): allow user to get workpace and delete

--- Backend/controllers/workpace.controller.js
+++ Backend/controllers/workpace.controller.js
@@ -25,11 +25,50 @@ export const createWorkSpace = async (req, res) => {
       message: "Workspace created successfully",
       workspace,
     });
-
   } catch (error) {
     return res.status(STATUS_CODE.INTERNAL_SERVER_ERROR).json({
       success: false,
       message: error.message,
     });
   }
+};
+
+// workspaceData
+export const getWorkspace = async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(STATUS_CODE.BAD_REQUEST).json({
+      success: false,
+      message: "user ID is required!",
+    });
+  }
+
+  try {
+    const workspace = await WorkspaceModel.find({ owner: id });
+
+    if(!workspace) {
+      return res.status(STATUS_CODE.BAD_REQUEST).json({
+        success: false,
+        message: "Workspace not found"
+      })
+    }
+
+    return res.status(STATUS_CODE.OK).json({
+      success: true,
+      message: "Workspace fetched succssfully",
+      workspace,
+    });
+  } catch (error) {
+    console.error("Error fetching workspaces:", error);
+    return res.status(STATUS_CODE.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Server error while fetching workspaces",
+    });
+  }
+};
+
+// deleteWorkpace
+export const deleteWorkspace = async (req, res) => {
+ 
 };
--- Backend/routes/workspace.routes.js
+++ Backend/routes/workspace.routes.js
@@ -1,7 +1,9 @@
 import express from "express"
 import {verifyToken} from '../middleware/auth.middleware.js'
-import { createWorkSpace } from "../controllers/workpace.controller.js"
+import { createWorkSpace, deleteWorkspace, getWorkspace } from "../controllers/workpace.controller.js"
 
 export const workspaceRoutes = express.Router()
 
 workspaceRoutes.post("/createWorkspace", verifyToken, createWorkSpace)
+workspaceRoutes.get("/getWorkspace/:id", verifyToken, getWorkspace)
+// workspaceRoutes.delete("/deleteWorkspace/:workpaceId", verifyToken, deleteWorkspace)
